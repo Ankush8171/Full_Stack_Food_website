afc711308558885f5b1d9dac637608dd797ccd79
Ankush22014 <ankush.chauhan@metasquaretech.com>
2026-02-23 13:12:00 +0530

    frontend is done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,37 @@
+
+
+import React, { useState } from 'react'
+import Navbar from './components/Navbar/Navbar'
+import { Route,Routes } from 'react-router-dom'
+import Cart from './pages/cart/Cart'
+import Home from './pages/Home/Home'
+import PlaceOrder from './pages/placeOrder/PlaceOrder'
+import Footer from './components/footer/Footer'
+import LoginPopup from './components/LoginPopup/LoginPopup'
+
+
+
+const App = () => {
+
+  const [showLogin,setShowLogin] = useState(false)
+  return (
+    <>
+    {showLogin ? <LoginPopup setShowLogin={setShowLogin}/>:<></>}
+    <div className='app'>
+      <Navbar setShowLogin={setShowLogin}/>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+         <Route path='/cart' element={<Cart/>}></Route>
+          <Route path='/order' element={<PlaceOrder/>}></Route>
+      </Routes>     
+    </div>
+    <Footer/>
+    
+    </>
+    
+    
+  )
+}
+
+export default App
+
