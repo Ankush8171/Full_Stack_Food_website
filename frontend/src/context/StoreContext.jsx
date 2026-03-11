68c20b798dfd71555a6a73dc0978b5d66569951b
Ankush22014 <ankush.chauhan@metasquaretech.com>
2026-03-11 22:43:45 +0530

    frontend api updated

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,13 +1,13 @@
-import { createContext, useEffect, useState } from "react";
-import { food_list } from "../assets/frontend_assets/assets";
-
+import axios from "axios";
+import { createContext, use, useEffect, useState } from "react";
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
   const [cartItems,setCartItems] = useState({});
-  const url = "http://localhost:5000/";
+  const url = "http://localhost:5000";
   const [token,setToken] = useState("");
+  const [food_list,setFoodList] = useState([]);
 
   const addToCart=(itemId)=>{
     if(!cartItems[itemId]){
@@ -35,6 +35,21 @@ const StoreContextProvider = (props) => {
 
     }   return totalAmount;
   }
+
+  const fetchFoodList = async ()=>{
+    const response = await axios.get(url+"/api/food/list");
+    setFoodList(response.data.data);
+  }
+
+  useEffect(()=>{
+    async function loadData(){
+      await fetchFoodList();
+      if(localStorage.getItem("token")){
+      setToken(localStorage.getItem("token"))
+    }
+    }
+    loadData();
+  },[])
 
     const contextValue={
         food_list,
